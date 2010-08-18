af32bc7186fc46305d34b33c346ecd191d33425c
Sergey Matveev <stargrave@users.sourceforge.net>
2010-08-18 16:14:15 +0400

    Added AGPL required notes about source code's download place.

--- media/js/configurator.js
+++ media/js/configurator.js
@@ -1,19 +1,23 @@
-// ETConf -- web-based user-friendly computer hardware configurator
-// Copyright (C) 2010 ETegro Technologies, PLC <http://www.etegro.com/>
-//                    Sergey Matveev <sergey.matveev@etegro.com>
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-// 
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/* 
+ * ETConf -- web-based user-friendly computer hardware configurator
+ * Copyright (C) 2010 ETegro Technologies, PLC <http://www.etegro.com/>
+ *                    Sergey Matveev <sergey.matveev@etegro.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can get source code for this engine here: http://sourceforge.net/p/etconf/home/
+ */
 
 var FLOWER_PATH = "/img/configurator/flower.gif";
 
