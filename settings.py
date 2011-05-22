7287b95b8157de5d38d28e9c4006caac9320453e
Sergey Matveev <stargrave@users.sourceforge.net>
2011-05-22 19:28:56 +0400

    Our default language is english Signed-off-by: Sergey Matveev <stargrave@users.sourceforge.net>

--- settings.py
+++ settings.py
@@ -1,25 +1,26 @@
+LANGUAGE_CODE = 'en'
 ################################################################################
 # Cofigurator related options
 ################################################################################
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DEBUG = True
 
-DATABASE_ENGINE = 'to be filled'
-DATABASE_NAME = 'to be filled'
-DATABASE_USER = 'to be filled'
-DATABASE_PASSWORD = 'to be filled'
-DATABASE_HOST = 'to be filled'
-DATABASE_PORT = 'to be filled'
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'sggal'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'root'
+DATABASE_HOST = ''
+DATABASE_PORT = ''
 
 CART_COOKIE_NAME = "configurator-cart"
 
-CACHE_BACKEND = 'to be filled'
-CACHE_MIDDLEWARE_SECONDS = 600
+CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+CACHE_MIDDLEWARE_SECONDS = 1800
 CACHE_MIDDLEWARE_KEY_PREFIX = ""
 
-IMAGES_PATH = 'to be filled'
-FONTS_PATH = 'to be filled'
-STYLE_PATH = 'to be filled'
+IMAGES_PATH = '/var/www/etegro2/www/img'
+FONTS_PATH = '/home/asapronov/fonts'
+STYLE_PATH = '/var/www/etegro2/configurator/media/rst2pdf-stylesheet.style'
 
 ORDER_SUBJECT_FROM = "etegro.com"
 ORDER_FROM = "sales@etegro.com"
@@ -30,7 +31,7 @@ ORDER_CC = "www@etegro.com"
 ################################################################################
 DEFAULT_CONTENT_TYPE = "text/html" # Should be "application/xhtml+xml"
 SESSION_COOKIE_NAME = "configurator-sessionid"
-LOGIN_URL = "/configurator/admin"
+LOGIN_URL = "/wizard/admin"
 
 ADMINS = (
 	('Sergey Matveev', 'sergey.matveev@etegro.com'),
@@ -40,13 +41,13 @@ AUTH_PROFILE_MODULE = "partners.PartnerProfile"
 MANAGERS = ADMINS
 
 TIME_ZONE = 'Europe/Moscow'
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en'
 SITE_ID = 1
 USE_I18N = True
 MEDIA_ROOT = ''
 MEDIA_URL = '/static/configurator/'
 ADMIN_MEDIA_PREFIX = '/static/configurator/admin/'
-SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+SECRET_KEY = 'fg)5$644e!=-ckg#*6aqghx-c#q7^i(%ui&8bdgq#whtseee-t'
 #SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_PATH = "/wizard"
 
